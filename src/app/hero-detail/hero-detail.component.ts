5168da1e5f052578021c83ce9196517657bd0f5b
Édipo Nascimento <ediposnascimento@gmail.com>
2020-05-01 11:58:07 -0300

    added angular router, redirect router, used routerLink directive

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { Location } from "@angular/common";
 
+import { HeroService } from "../hero.service";
 import { Hero } from "../hero";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-hero-detail",
@@ -8,9 +11,24 @@ import { Hero } from "../hero";
   styleUrls: ["./hero-detail.component.css"],
 })
 export class HeroDetailComponent implements OnInit {
-  constructor() {}
+  hero: Hero;
 
-  @Input() hero: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,22 +12,13 @@ import { MessageService } from "../message.service";
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
-    this.hetHeroes();
+    this.getHeroes();
   }
 
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-  }
-
-  hetHeroes(): void {
+  getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
